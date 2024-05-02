41fe075f59867ec968a7fb586a9a8b95d07e04ff
vikash mishra <vikashmishra8371@gmail.com>
2024-05-02 22:19:30 +0530

    ls

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,6 +1,6 @@
 import Loader from 'react-js-loader'
 export const Loading = ()=>{
-    return <div style={{position:'fixed',top:'50%',left:"45%",padding:'20px',background:'black'}} >
+    return <div style={{position:'fixed',top:'50%',margin:'auto',padding:'20px',background:'black'}} >
 
   <Loader type="spinner-circle" bgColor={'white'} />
  <span style={{color:'white'}}>Please wait....</span> 
